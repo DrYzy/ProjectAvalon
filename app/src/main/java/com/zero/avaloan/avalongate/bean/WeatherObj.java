9df455ad55bb5c28b385ebcb19fe19859db940f9
yzy <Laketony@laketony-mac.local>
2019-07-27 14:34:53 +0800

    retrofit接入

---
+++ app/src/main/java/com/zero/avaloan/avalongate/bean/WeatherObj.java
@@ -0,0 +1,245 @@
+package com.zero.avaloan.avalongate.bean;
+
+import java.util.List;
+
+public class WeatherObj {
+    private String date;
+    private String message;
+    private int status;
+    private String city;
+    private int count;
+    private WeatherBean data;
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public WeatherBean getData() {
+        return data;
+    }
+
+    public void setData(WeatherBean data) {
+        this.data = data;
+    }
+
+
+
+    public class WeatherBean {
+        private String shidu;
+        private int pm25;
+        private int pm10;
+        private String quality;
+        private String wendu;
+        private String ganmao;
+        private YesterdayBean yesterday;
+
+        public String getShidu() {
+            return shidu;
+        }
+
+        public void setShidu(String shidu) {
+            this.shidu = shidu;
+        }
+
+        public int getPm25() {
+            return pm25;
+        }
+
+        public void setPm25(int pm25) {
+            this.pm25 = pm25;
+        }
+
+        public int getPm10() {
+            return pm10;
+        }
+
+        public void setPm10(int pm10) {
+            this.pm10 = pm10;
+        }
+
+        public String getQuality() {
+            return quality;
+        }
+
+        public void setQuality(String quality) {
+            this.quality = quality;
+        }
+
+        public String getWendu() {
+            return wendu;
+        }
+
+        public void setWendu(String wendu) {
+            this.wendu = wendu;
+        }
+
+        public String getGanmao() {
+            return ganmao;
+        }
+
+        public void setGanmao(String ganmao) {
+            this.ganmao = ganmao;
+        }
+
+        public YesterdayBean getYesterday() {
+            return yesterday;
+        }
+
+        public void setYesterday(YesterdayBean yesterday) {
+            this.yesterday = yesterday;
+        }
+
+        public List<ForecastBean> getForecast() {
+            return forecast;
+        }
+
+        public void setForecast(List<ForecastBean> forecast) {
+            this.forecast = forecast;
+        }
+
+        private List<ForecastBean> forecast;
+
+        public class YesterdayBean {
+            private String date;
+            private String sunrise;
+            private String high;
+            private String low;
+            private String sunset;
+            private int aqi;
+            private String fx;
+            private String fl;
+            private String type;
+            private String notice;
+        }
+
+        public class ForecastBean {
+            private String date;
+            private String sunrise;
+            private String high;
+            private String low;
+            private String sunset;
+            private int aqi;
+            private String fx;
+            private String fl;
+            private String type;
+
+            public String getDate() {
+                return date;
+            }
+
+            public void setDate(String date) {
+                this.date = date;
+            }
+
+            public String getSunrise() {
+                return sunrise;
+            }
+
+            public void setSunrise(String sunrise) {
+                this.sunrise = sunrise;
+            }
+
+            public String getHigh() {
+                return high;
+            }
+
+            public void setHigh(String high) {
+                this.high = high;
+            }
+
+            public String getLow() {
+                return low;
+            }
+
+            public void setLow(String low) {
+                this.low = low;
+            }
+
+            public String getSunset() {
+                return sunset;
+            }
+
+            public void setSunset(String sunset) {
+                this.sunset = sunset;
+            }
+
+            public int getAqi() {
+                return aqi;
+            }
+
+            public void setAqi(int aqi) {
+                this.aqi = aqi;
+            }
+
+            public String getFx() {
+                return fx;
+            }
+
+            public void setFx(String fx) {
+                this.fx = fx;
+            }
+
+            public String getFl() {
+                return fl;
+            }
+
+            public void setFl(String fl) {
+                this.fl = fl;
+            }
+
+            public String getType() {
+                return type;
+            }
+
+            public void setType(String type) {
+                this.type = type;
+            }
+
+            public String getNotice() {
+                return notice;
+            }
+
+            public void setNotice(String notice) {
+                this.notice = notice;
+            }
+
+            private String notice;
+        }
+    }
+}
